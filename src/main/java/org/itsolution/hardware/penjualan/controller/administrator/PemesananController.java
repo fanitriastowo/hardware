460d8316fb2993abe0770523ac5d38711b721998
fanitriastowo <fani.triastowo@gmail.com>
2016-07-22 00:20:15 +0700

    fixing checkout bug, change image url pemesanan report and analytic

--- src/main/java/org/itsolution/hardware/penjualan/controller/administrator/PemesananController.java
+++ src/main/java/org/itsolution/hardware/penjualan/controller/administrator/PemesananController.java
@@ -3,6 +3,8 @@ package org.itsolution.hardware.penjualan.controller.administrator;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.itsolution.hardware.penjualan.entity.PemesananEntity;
 import org.itsolution.hardware.penjualan.service.PemesananService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +18,29 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/administrator/pemesanan")
 public class PemesananController {
 
-	@Autowired
-	private PemesananService pemesananService;
+    @Autowired
+    private PemesananService pemesananService;
 
-	@RequestMapping
-	public ModelAndView index() {
-		ModelAndView mav = new ModelAndView("administrator/administrator-pemesanan");
-		mav.addObject("pemesanans", pemesananService.findAllOrderByTransfer());
-		return mav;
-	}
+    @RequestMapping
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("administrator/administrator-pemesanan");
+        mav.addObject("pemesanans", pemesananService.findAllOrderByTransfer());
+        return mav;
+    }
 
-	@RequestMapping("/cetak/{id}")
-	public String cetakPemesanan(@PathVariable("id") Integer id, ModelMap modelMap) {
-		List<PemesananEntity> listPemesananEntity = new ArrayList<>();
-		listPemesananEntity.add(pemesananService.findOne(id));
-		modelMap.addAttribute("dataSource", listPemesananEntity);
-		return "pemesanan";
-	}
+    @RequestMapping("/cetak/{id}")
+    public ModelAndView cetakPemesanan(@PathVariable("id") Integer id, ModelMap modelMap, HttpServletRequest request) {
+
+        PemesananEntity pemesananEntity = pemesananService.findOne(id);
+        List<PemesananEntity> list = new ArrayList<>();
+        list.add(pemesananEntity);
+
+        modelMap.addAttribute("dataSource", list);
+        modelMap.addAttribute("url", request.getScheme() + "://" + request.getServerName() + ":"
+                + request.getServerPort() + request.getContextPath());
+        System.out.println(request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+                + request.getContextPath());
+
+        return new ModelAndView("pemesanan", modelMap);
+    }
 }
---
+++ src/test/java/org/itsolution/hardware/penjualan/test/controller/front/PemesananControllerTest.java
@@ -0,0 +1,58 @@
+package org.itsolution.hardware.penjualan.test.controller.front;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/dispatcher-servlet.xml",
+        "file:src/main/webapp/WEB-INF/applicationContext.xml" })
+@WebAppConfiguration
+@Transactional
+@ActiveProfiles("dev")
+public class PemesananControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testAksesDaftarBrand() {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        try {
+            this.mockMvc.perform(MockMvcRequestBuilders.get("/administrator/pemesanan/cetak/{id}", 1))
+
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+
+                    .andExpect(MockMvcResultMatchers.model().attribute("url", request.getScheme() + "://"
+                            + request.getServerName() + ":" + request.getServerPort() + "/" + request.getContextPath()))
+
+                    .andExpect(MockMvcResultMatchers.view().name("pemesanan"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+}
